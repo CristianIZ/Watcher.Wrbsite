3c64b88ce13effb2f12d4a3a37273286143e532c
navegador <navegador@05000220-70902>
2019-09-19 23:47:48 -0300

    push inicial

---
+++ safari-castelar-master/Business/Safari.Business/PetComponent.cs
@@ -0,0 +1,54 @@
+﻿using Safari.Data;
+using Safari.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safari.Business
+{
+    public class PetComponent
+    {
+        public Pet Agregar(Pet pet)
+        {
+            Pet result = default(Pet);
+            var petDAC = new PetDAC();
+
+            result = petDAC.Create(pet);
+            return result;
+        }
+
+        public Pet ObtenerMascota(int id)
+        {
+            Pet result = default(Pet);
+            var petDAC = new PetDAC();
+
+            result = petDAC.ReadBy(id);
+            return result;
+        }
+
+        public List<Pet> ListarTodos()
+        {
+            List<Pet> result = default(List<Pet>);
+
+            var petDAC = new PetDAC();
+            result = petDAC.Read();
+            return result;
+
+        }
+
+        public void ModificarMascota(Pet pet)
+        {
+            var petDAC = new PetDAC();
+            petDAC.Update(pet);
+        }
+
+        public void BorrarMascota(Pet pet)
+        {
+            var petDAC = new PetDAC();
+            petDAC.Delete(pet.Id);
+        }
+
+    }
+}
---
+++ safari-castelar-master/IOC/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Safari.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC
+{
+    public class Class1 : IEspecie
+    {
+        public void CrearEspecie()
+        {
+
+        }
+    }
+}
